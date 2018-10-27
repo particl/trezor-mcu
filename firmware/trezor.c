d16b287cda44c1d612e1c11f8210bf89082af572
tecnovert <tecnovert@particl.io>
2018-10-27 10:21:38 +0200

    Add Particl

--- firmware/trezor.c
+++ firmware/trezor.c
@@ -91,7 +91,7 @@ int main(void)
 	__stack_chk_guard = random32(); // this supports compiler provided unpredictable stack protection checks
 	oledInit();
 #else
-	check_bootloader();
+	//check_bootloader();
 	setupApp();
 	__stack_chk_guard = random32(); // this supports compiler provided unpredictable stack protection checks
 #endif
